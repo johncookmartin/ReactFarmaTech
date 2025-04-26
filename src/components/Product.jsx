86081a51e56705d3b00c2f80692449af47649361
John Cook-Martin <johncookmartin@gmail.com>
2025-04-26 11:52:23 -0400

    Made Product Tiles Expandable

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,36 +1,63 @@
 import React from 'react';
+import { useState } from 'react';
 import ProductTable from './ProductTable';
 import { nanoid } from 'nanoid';
 import productImage from '../assets/products/stock/FT 12.jpg';
 import './styles/product.css';
 
 const Product = ({ data }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const productFormulations = data.formulations.map((formulation) => ({
     ...formulation,
     id: nanoid(),
   }));
 
+  const toggleExpand = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
   return (
     <article className="product-container">
       <div className="product-container-header">
         <img src={productImage} />
-        <h3 className="product-name">{data.name}</h3>
+        <h3 className="product-name" onClick={toggleExpand}>
+          {data.name}
+        </h3>
       </div>
-      <div className="product-container-body">
-        <div className="product-formulations">
-          {productFormulations.map((formulation) => (
-            <ProductTable key={formulation.id} data={formulation} />
-          ))}
+
+      {isExpanded && (
+        <div className="product-container-body">
+          <div className="product-formulations">
+            {productFormulations.map((formulation) => (
+              <ProductTable key={formulation.id} data={formulation} />
+            ))}
+          </div>
+          {data.target_pests && (
+            <div className="product-target-pests">
+              <table className="product-specs-table">
+                <thead>
+                  <tr>
+                    <th>Target Pests</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.target_pests.map((pest) => (
+                    <tr key={pest}>
+                      <td>{pest}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+          <div className="product-special-instructions">
+            <p>
+              <b>Special Instructions:</b> {data.special_instructions}
+            </p>
+          </div>
         </div>
-        <div className="product-target-pests">
-          <ProductTable />
-        </div>
-        <div className="product-special-instructions">
-          <p>
-            <b>Special Instructions:</b> {data.special_instructions}
-          </p>
-        </div>
-      </div>
+      )}
     </article>
   );
 };
